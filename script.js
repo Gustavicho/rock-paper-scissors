ef71a7cb1e226e48376c101da5416dcf14dca2cc
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-06-12 23:43:58 -0400

    Feat: add func to get computer choice

---
+++ script.js
@@ -0,0 +1,16 @@
+const userChoice = document.querySelector("#choice")
+
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3) + 1;
+
+    switch (num) {
+        case 1:
+            return "rock"
+    
+        case 2:
+            return "paper"
+
+        case 3:
+            return "scis"
+    }
+}
