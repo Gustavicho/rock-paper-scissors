979d264f1a8743efe685b2fb89a6a41b190c5568
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-06-18 17:06:47 -0400

    Style: chage the indetion from 4 to 2 spaces

--- script.js
+++ script.js
@@ -5,37 +5,37 @@ let computer;
 let user;
 
 document.querySelector("button").addEventListener("click", () => {
-    user = getUserChoice();
-    computer = getComputerChoice();
-    let resultText;
+  user = getUserChoice();
+  computer = getComputerChoice();
+  let resultText;
 
-    if (user === computer) {
-        resultText = "Draw!"
-    } else if (
-        user === "rock" && computer === "scis" ||
-        user === "scis" && computer === "paper" ||
-        user === "paper" && computer === "rock" )
-    {
-        resultText = "YOU WIN!!!"
-    } else {
-        resultText = "The computer wins..."
-    }
-    result.textContent = resultText;
+  if (user === computer) {
+    resultText = "Draw!"
+  } else if (
+    user === "rock" && computer === "scis" ||
+    user === "scis" && computer === "paper" ||
+    user === "paper" && computer === "rock" )
+  {
+    resultText = "YOU WIN!!!"
+  } else {
+    resultText = "The computer wins..."
+  }
+  result.textContent = resultText;
 });
 
 function getComputerChoice() {
-    let num = Math.floor(Math.random() * 3) + 1;
+  let num = Math.floor(Math.random() * 3) + 1;
 
-    switch (num) {
-        case 1:
-            return "rock"
-    
-        case 2:
-            return "paper"
+  switch (num) {
+    case 1:
+      return "rock"
 
-        case 3:
-            return "scis"
-    }
+    case 2:
+      return "paper"
+
+    case 3:
+      return "scis"
+  }
 }
 
 function getUserChoice() {
