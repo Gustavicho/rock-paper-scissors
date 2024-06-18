002db685973912da23bc14ecaf6a6a1eb3294a6b
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-06-18 17:37:16 -0400

    Refac: adapt code to work on bts

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
-const userChoice = document.querySelector("#choice")
+const choicesBtns = document.querySelectorAll(".btn--choice")
 const result = document.querySelector(".results")
 
 let computer;
 let user;
 
-document.querySelector("button").addEventListener("click", () => {
-  user = getUserChoice();
-  computer = getComputerChoice();
+choicesBtns.forEach(btn => {
+  btn.addEventListener('click', event => {
+    playRound(event);
+  });
+});
+
+/* ----- FUNCTIONS ----- */
+function playRound(e) {
+  const winCondition = {
+    rock: "scis",
+    scis: "paper",
+    paper: "rock"
+  };
   let resultText;
+  user = getUserChoice(e);
+  computer = getComputerChoice();
 
   if (user === computer) {
     resultText = "Draw!"
-  } else if (
-    user === "rock" && computer === "scis" ||
-    user === "scis" && computer === "paper" ||
-    user === "paper" && computer === "rock" )
-  {
+  } else if (winCondition[user] === computer) {
     resultText = "YOU WIN!!!"
   } else {
     resultText = "The computer wins..."
   }
   result.textContent = resultText;
-});
+}
 
 function getComputerChoice() {
   let num = Math.floor(Math.random() * 3) + 1;
 
   switch (num) {
     case 1:
-      return "rock"
+      return "rock";
 
     case 2:
-      return "paper"
+      return "paper";
 
     case 3:
-      return "scis"
+      return "scis";
   }
 }
 
-function getUserChoice() {
-    return userChoice.value
+function getUserChoice(e) {
+    return e.target.value;
 }
